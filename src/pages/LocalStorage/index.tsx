53e3f144c2294f62b3e1636528f4da94328aa911
Gurgen Torosyan <gurgentorosyan88@gmail.com>
2024-03-05 23:52:11 +0400

    Add LocalStorageItem

--- src/pages/LocalStorage/index.tsx
+++ src/pages/LocalStorage/index.tsx
@@ -1,15 +1,69 @@
-import { Accordion, AccordionDetails, AccordionSummary, Box, Button, Card, Container, Typography } from '@mui/material'
+import { Accordion, AccordionDetails, AccordionSummary, Box, Button, Card, Container, TextField, Typography } from '@mui/material'
 import ExpandMoreIcon  from '@mui/icons-material/ExpandMore';
 import './LocalStorage.css'
+import { useEffect, useRef, useState } from 'react';
 
+interface LocalStorageItem {
+  data: { [key: string]: string }[];
+  expiration: number;
+  creation: number;
+}
 
 export const LocalStorage = () => {
+  let timer = useRef< NodeJS.Timeout>();
+  const [discountTime, setDiscountTime] = useState(10000)
+  const discount: LocalStorageItem = {
+    data: [{ discount: '10%' }],
+    creation: Date.now(),
+    expiration: Date.now() + discountTime
+  };
+
+
+  const handleSetDiscount = () => {
+    clearInterval(timer.current)
+    localStorage.setItem('discount', JSON.stringify(discount));
+
+    updateCount();
+  }
+
+  useEffect(() => {
+    const localStorageData = (localStorage.getItem('discount'));
+
+    if (localStorageData) {
+      const currentDiscount: LocalStorageItem = JSON.parse(localStorageData);
+      if (Date.now() > currentDiscount.expiration) {
+        localStorage.removeItem('discount')
+      }
+    }
+
+    return () => {
+      clearInterval(timer.current)
+    }
+  }, [])
+  
+  const updateCount = () => {
+    setDiscountTime(10000)
+
+    timer.current = setInterval(() => {
+      setDiscountTime(prevCount => {
+        if (prevCount <= 0) {
+          clearInterval(timer.current)
+          return 0
+        }
+        return prevCount - 1000
+      })
+    }, 1000);
+  }
 
   return (
     <Container>
       <Card sx={{ m: 5 }}>
         <Box  sx={{ mb: 5, p: 2, display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
           <Button variant='outlined' color='error' onClick={() => localStorage.clear()}>Clear Local Storage</Button>
+          <Button variant='outlined' color='info' onClick={handleSetDiscount}>{`Set Discount Value (${discountTime} seconds)`}</Button>
+        </Box>
+        <Box>
+          <Button onClick={handleSetDiscount}>Add discount</Button>
         </Box>
         <Accordion>
           <AccordionSummary
